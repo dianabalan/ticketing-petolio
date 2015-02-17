e49ecf013ec3e41bdf1d2b9b384df191f2a90bd6
k.arpi207@gmail.com <k.arpi207@gmail.com@ba4ab901-9201-6d8b-66d7-97f4924ecefe>
2015-02-17 21:17:09 +0000

    Fixed interface: show coresponding menu for different user types.

--- branches/ticketing/application/modules/frontend/controllers/TicketsController.php
+++ branches/ticketing/application/modules/frontend/controllers/TicketsController.php
@@ -25,6 +25,8 @@ class TicketsController extends Zend_Controller_Action
 	}
 	
 	public function preDispatch() {
+		$this->verifyUser();
+		$this->verifyRole();		
 	}
 	
 	public function postDispatch() {
@@ -41,39 +43,48 @@ class TicketsController extends Zend_Controller_Action
 			return $this->_helper->redirector('index', 'site');
 		}
 	}
+	
+	/*
+	 * @author Stefan Baiu
+	 */	
+	private function verifyRole()
+	{
+		$action_name = $this->request->getActionName();
+		$usrType = $this->auth->getIdentity()->type;
+	
+		if ( $action_name != "my-tickets" && $usrType == 1 )
+		{
+			Petolio_Service_Util::saveRequest();
+            $this->msg->messages[] = $this->translate->_("Please log in or sign up as 'Service Provider' to access this page");
+            return $this->_helper->redirector('index', 'site');
+		}
+	}
 
 	public function myTicketsAction() {
-		$this->verifyUser();		
 		$this->view->title = $this->translate->_("My Tickets");
 	}
 	
 	public function addTicketAction() {
-		$this->verifyUser();
 		$this->view->title = $this->translate->_("Add Ticket");
 	}
 	
 	public function ticketsArchivesAction() {
-		$this->verifyUser();
 		$this->view->title = $this->translate->_("Tickets Archives");
 	}
 	
 	public function myClientsAction() {
-		$this->verifyUser();
 		$this->view->title = $this->translate->_("My Clients");
 	}
 	
 	public function addClientAction() {
-		$this->verifyUser();
 		$this->view->title = $this->translate->_("Add Client");
 	}
 	
 	public function manageNonPetolioMembersAction() {
-		$this->verifyUser();
 		$this->view->title = $this->translate->_("Manage Non-Petolio Members");
 	}
 	
 	public function clientsArchivesAction() {
-		$this->verifyUser();
 		$this->view->title = $this->translate->_("Clients Archives");
 	}
 }
