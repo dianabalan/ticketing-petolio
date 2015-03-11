21d44d7e608937f0998dc7a52b20482b3cac5120
k.arpi207@gmail.com <k.arpi207@gmail.com@ba4ab901-9201-6d8b-66d7-97f4924ecefe>
2015-03-11 11:00:57 +0000

    Added functionality to add-ticket view: added combo-box, and listing of services and products, based on selection.

---
+++ application/forms/TicketAdd.php
@@ -0,0 +1,52 @@
+<?php
+class Petolio_Form_TicketAdd extends Petolio_Form_Main
+{
+	private $category = null;
+	
+	/**
+	 * Form for AddTicket.
+	 * 
+	 * @param string $category Sets combo-box category, default is "empty".
+	 */
+	public function __construct($category = "empty")
+	{
+		$this->category = $category;
+		parent::__construct();
+	}
+
+	public function init()
+	{
+		$translate = Zend_Registry::get('Zend_Translate');
+		
+		/* Form Elements & Other Definitions Here ... */
+		$this->setDecorators(array('FormElements','Form'));
+		$this->setElementDecorators(
+				array('PoStandardElement')
+		);
+		$this->removeDecorator('DtDdWrapper');
+		$this->addElementPrefixPaths(array(
+				'decorator' => array('Petolio_Decorator' => APPLICATION_PATH.'/decorators'),
+				'validate' => array('Petolio_Validator_' => APPLICATION_PATH.'/forms/validators/')
+		));
+		
+		// set method
+		$this->setMethod(Zend_Form::METHOD_POST);
+		 
+		// handle errors
+		$errors = $this->admin ? array() : array('errors_class' => 'cluetip_errors', 'msg_errors' => true);
+		
+		$type = array(
+				"prod"=> $translate->_("Products"),
+				"serv"=> $translate->_("Services")
+		);
+		
+		$this->addElement('select', 'attribute_set', array(
+				'label' => $translate->_('Category'),
+				'attribs' => array('empty' => $translate->_('Select a Category')),
+				'multiOptions' => $type,
+				'value' => $this->category,
+				'required' => true,
+	        	'html' => 'onchange="showData(this)"'
+		));
+	}
+}
