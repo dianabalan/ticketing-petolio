1d0d22c09d4ca4bcb92d8d85c397b6520158a213
k.arpi207@gmail.com <k.arpi207@gmail.com@ba4ab901-9201-6d8b-66d7-97f4924ecefe>
2015-03-13 14:04:24 +0000

    Implemented: Ticket displaying, editing and archiveing.

--- branches/ticketing/application/models/DbTable/Ticket/Tickets.php
+++ branches/ticketing/application/models/DbTable/Ticket/Tickets.php
@@ -7,27 +7,25 @@ class Petolio_Model_DbTable_Ticket_Tickets extends Zend_Db_Table_Abstract
 
 	protected $_primary = 'ID';
 	
+	protected static $_columns = array(
+			'ID',
+			'item_id',
+			'user_id',
+			'description',
+			'ticket_date',
+			'date_created',
+			'date_modified',
+			'flgreminder',
+			'archive',
+			'scope'
+	);
+	
 	public function fetchTicket($id)
 	{
 		$db = $this->getAdapter();
 	
-		$columns = array(
-				'ID',
-				'item_id',
-				'user_id',
-				'service',
-				'amount',
-				'price',
-				'ticket_date',
-				'date_created',
-				'date_modified',
-				'flgreminder',
-				'archive',
-				'scope'
-		);
-	
 		$query = $db->select()
-		->from($this->_name, $columns)
+		->from($this->_name, self::$_columns)
 		->where('ID = :id');
 	
 		$row = $db->fetchRow($query, array(
@@ -35,6 +33,23 @@ class Petolio_Model_DbTable_Ticket_Tickets extends Zend_Db_Table_Abstract
 		));
 	
 		return $row;
+	}
+
+	public function fetchTickets($user_id, $archive)
+	{
+		$db = $this->getAdapter();
+	
+		$query = $db->select()
+			->from($this->_name, self::$_columns)
+			->where('user_id = :user_id')
+			->where('archive = :archive');
+	
+		$rows = $db->fetchAll($query, array(
+				':user_id' => $user_id,
+				':archive' => $archive
+		));
+	
+		return $rows;
 	}
 	
 	public function insert(array $data)
